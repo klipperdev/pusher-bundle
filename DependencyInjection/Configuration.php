5fbc8fa69acc362b2397f2ae74135aed11170704
François Pluchino <francois.pluchino@klipper.dev>
2022-04-02 17:31:29 +0200

    Update php code style

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,6 +23,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('klipper_pusher');
+
         /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
         $rootNode
